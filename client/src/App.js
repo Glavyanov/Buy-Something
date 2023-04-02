1e5c335795bd5cac0993cd7be21f69f30932af8a
Glavyanov <dimitarglavianov@gmail.com>
2023-04-02 14:29:47 +0300

    add permission to append ad

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+import { AuthProvider } from "./contexts/AuthenticationContext";
+import { AdProvider } from "./contexts/AdContext";
 
 import "./App.css";
 
@@ -10,54 +12,38 @@ import { Login } from "./components/Login/Login";
 import { AppendAd } from "./components/MyPage/AppendAd/AppendAd";
 import { MyPage } from "./components/MyPage/MyPage";
 import { Register } from "./components/Register/Register";
-import * as cardsService from "./services/cardsServise";
 
 function App() {
-  const [ads, setAds] = useState([]);
-
-  useEffect(() => {
-    cardsService.getSevenLatest().then((res) => setAds(res));
-  }, []);
-
-  const navigate = useNavigate();
-
-  const onLoginSubmit = (e) => {
-    e.preventDefault();
-    console.log("onLoginSubmit");
-    navigate('/mypage');
-  };
 
   const onRegisterSubmit = (e) => {
     e.preventDefault();
     console.log("onRegisterSubmit");
   };
-
-  const onSubmitAppendAd = (values) => {
-    console.log(values);
-  };
-
+  
   return (
-    <>
-      <Header />
+    <AuthProvider>
+      <AdProvider>
+        <Header />
 
-      <main id="main">
-        <Routes>
-          <Route path="/" element={<Home ads={ads} />} />
-          <Route
-            path="/login"
-            element={<Login onLoginSubmit={onLoginSubmit} />}
-          />
-          <Route
-            path="/register"
-            element={<Register onRegisterSubmit={onRegisterSubmit} />}
-          />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/appendad" element={<AppendAd onSubmitAppendAd={onSubmitAppendAd}/>} />
-        </Routes>
-      </main>
+        <main id="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/register"
+              element={<Register onRegisterSubmit={onRegisterSubmit} />}
+            />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route
+              path="/appendad"
+              element={<AppendAd />}
+            />
+          </Routes>
+        </main>
 
-      <Footer />
-    </>
+        <Footer />
+      </AdProvider>
+    </AuthProvider>
   );
 }
 
