a49c6c462d711acafb9fceefb9351ceceb29c3f2
Glavyanov <dimitarglavianov@gmail.com>
2023-04-03 21:52:39 +0300

    handle error when local storage is not clean

--- client/src/contexts/AdContext.js
+++ client/src/contexts/AdContext.js
@@ -1,43 +1,44 @@
-import { createContext, useState, useEffect, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import {createCard, getSevenLatest} from '../services/cardsServise';
+import { createContext, useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { createCard, getSevenLatest } from "../services/cardsServise";
 
 export const AdContext = createContext();
 
-export const AdProvider = ({
-    children,
-}) => {
-    const navigate = useNavigate();
-    const [ads, setAds] = useState([]);
-    useEffect(() => {
-        getSevenLatest()
-            .then(result => {
-                setAds(result)
-            })
-    }, []);
+export const AdProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [ads, setAds] = useState([]);
+  useEffect(() => {
+    getSevenLatest()
+      .then((result) => {
+        setAds(result);
+      })
+      .catch((err) => {
+        getSevenLatest().then((result) => {
+          setAds(result);
+        });
+      });
+  }, []);
 
-    const onCreateAdSubmit = async (data) => {
-        const newAd = await createCard(data);
+  const onCreateAdSubmit = async (data) => {
+    const newAd = await createCard(data);
 
-        setAds(state => [...state, newAd]);
+    setAds((state) => [...state, newAd]);
 
-        navigate('/');
-    };
+    navigate("/");
+  };
 
-    const contextValues = {
-        ads,
-        onCreateAdSubmit,
-    };
+  const contextValues = {
+    ads,
+    onCreateAdSubmit,
+  };
 
-    return (
-        <AdContext.Provider value={contextValues}>
-            {children}
-        </AdContext.Provider>
-    );
+  return (
+    <AdContext.Provider value={contextValues}>{children}</AdContext.Provider>
+  );
 };
 
 export const useAdContext = () => {
-    const context = useContext(AdContext);
+  const context = useContext(AdContext);
 
-    return context;
+  return context;
 };
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,63 +1,61 @@
-import {getUserData} from './util.js';
+import { getUserData } from "./util.js";
 
-const host = 'http://localhost:3030';
-
+const host = "http://localhost:3030";
 
 async function request(url, options) {
-    try {
-        const responce = await fetch(host + url, options);
+  try {
+    const responce = await fetch(host + url, options);
 
-        if (responce.ok !== true) {
-            if (responce.status === 403) {
-                localStorage.removeItem('userData');
-            }
-            const error = await responce.json();
-            throw new Error(error.message);
-        }
-
-        if (responce.status === 204) {
-            return responce;
-        } else {
-            return responce.json();
-        }
-
-    } catch (err) {
-        alert(err.message);
-        throw err;
+    if (responce.ok !== true) {
+      if (responce.status === 403) {
+        localStorage.removeItem("userData");
+      }
+      const error = await responce.json();
+      throw new Error(error.message);
     }
+
+    if (responce.status === 204) {
+      return responce;
+    } else {
+      return responce.json();
+    }
+  } catch (err) {
+    alert(err.message);
+    throw err;
+  }
 }
 
-function createOptions(method = 'get', data) {
-    const options = {
-        method,
-        headers: {}
-    };
+function createOptions(method = "get", data) {
+  const options = {
+    method,
+    headers: {},
+  };
 
-    if (data !== undefined) {
-        options.headers['Content-Type'] = 'application/json';
-        options.body = JSON.stringify(data);
-    }
+  if (data !== undefined) {
+    options.headers["Content-Type"] = "application/json";
+    options.body = JSON.stringify(data);
+  }
 
-    const userData = getUserData();
-    if (userData != null) {
-        options.headers['X-Authorization'] = userData.accessToken;
-    }
+  const userData = getUserData();
+  if (userData != null) {
+    options.headers["X-Authorization"] = userData.accessToken;
+  }
 
-    return options;
+  return options;
 }
 
 export async function get(url) {
-    return request(url, createOptions());
+  return request(url, createOptions());
 }
 
 export async function post(url, data) {
-    return request(url, createOptions('post', data));
+  return request(url, createOptions("post", data));
 }
 
 export async function put(url, data) {
-    return request(url, createOptions('put', data));
+  return request(url, createOptions("put", data));
 }
 
 export async function del(url) {
-    return request(url, createOptions('delete'));
+  return request(url, createOptions("delete"));
 }
