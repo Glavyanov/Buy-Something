32847f33c2cf899fd3307f384d01bb78b6d8f251
Glavyanov <dimitarglavianov@gmail.com>
2023-04-08 17:55:30 +0300

    fix card delete

--- client/src/components/CardDelete/CardDelete.js
+++ client/src/components/CardDelete/CardDelete.js
@@ -2,7 +2,8 @@ import "./CardDelete.css";
 
 export const CardDelete = ({ onDelete, onCloseHandler }) => {
   return (
-    <div onClick={onCloseHandler} id="card-del-back">
+    <>
+      <div onClick={onCloseHandler} id="card-del-back"></div>
       <div id="dialog">
         <p>Are you sure you want to delete this record?</p>
         <div id="dialogBtn">
@@ -10,6 +11,6 @@ export const CardDelete = ({ onDelete, onCloseHandler }) => {
           <button onClick={onCloseHandler}>Cancel</button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
