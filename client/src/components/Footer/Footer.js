67761bab3993be6569cc76d62ede9eec528631c6
Glavyanov <dimitarglavianov@gmail.com>
2023-04-08 16:45:53 +0300

    fix footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,7 +1,21 @@
-export const Footer = () => {
-    return (
-        <section className="footer">
+import "./Footer.css";
 
-        </section>
-    );
-}
+export const Footer = () => {
+  return (
+    <>
+      <section className="footer">
+        <div>&#174; 2023 - By Something</div>
+        <p>
+          Do you have any questions? <b>Email us</b>
+        </p>
+      </section>
+      <a href="#">
+        <div id="fixed">
+          <div id="fixed-flex">
+            <i class="fa-solid fa-angles-up fa-2xl"></i>
+          </div>
+        </div>
+      </a>
+    </>
+  );
+};
