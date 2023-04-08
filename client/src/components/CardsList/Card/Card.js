0ec963fee9f5209d621521c019a7ab7ea0a10c90
Glavyanov <dimitarglavianov@gmail.com>
2023-04-08 10:34:14 +0300

    add details button and login logic

--- client/src/components/CardsList/Card/Card.js
+++ client/src/components/CardsList/Card/Card.js
@@ -26,7 +26,7 @@ export const Card = ({ summary, imageUrl, category, title, _id, owner }) => {
       </section>
       <footer>
         <p>{category}</p>
-        {isAuthenticated ? <Link to={`/details/${_id}`}><button id="btn-details">See details</button></Link> : null}
+        {isAuthenticated ? <Link to={`/details/${_id}`}><button id="btn-details">See details</button></Link> : <Link to="/login"><button id="btn-details">See details</button></Link>}
         {owner ? <button id="btn-details" className="btn-delete" onClick={onClickConfirmDelete}><a href="#root">Delete</a></button>: null}
       </footer>
       {showDeleteCard && <CardDelete onCloseHandler={onCloseHandler} onDelete={() => onClickDelete(_id)} />}
